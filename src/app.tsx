41f65f928d3505309b850758a620c9701a00c5d6
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-13 20:07:32 +0900

    New | react 테스트하기 위한 파일

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>Welcome to the simple React test page.</p>
+    </div>
+  );
+};
+
+export default App;
