5abf2efd586081f2a0025eb6858e56ffde3b01b1
jaemin1005 <ghkdwoals1005@gmail.com>
2024-10-13 20:07:46 +0900

    New | 메인 진입점

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+}
