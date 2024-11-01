fc795b8f30c67061f5e6c72d5c4dd60e5decdcf2
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-01 23:00:57 +0900

    New | 현재 커밋과 레포를 보여주는 컴포넌트
    Commit과 Repo를 시간 순서대로 정렬하며, (Commit은 만들어진 날짜, Repo 업데이트 날짜)
    페이지 네이션을 통해 여러 정보들을 볼 수 있다.

---
+++ src/app/components/github_info.component.tsx
@@ -0,0 +1,110 @@
+import { Breadcrumbs, Button, Pagination } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useGitState } from "../../../context/git_state.context";
+import { Commit, Repository } from "../../../interfaces/git_state.interface";
+import { GitHubLabel } from "./github_label.component";
+
+type Select = "Commits" | "Repository";
+
+export function GitHubInfo() {
+  const [select, setSelect] = useState<Select>("Commits");
+  const [commits, setCommits] = useState<Commit[]>([]);
+  const [repos, setRepos] = useState<Repository[]>([]);
+  const [page, setPage] = useState(1);
+
+  const gitData = useGitState();
+  const itemsPerPage = 6;
+
+  // 데이터 시간순서대로 정렬
+  useEffect(() => {
+    const commits = gitData
+      .map((value) => value.commits)
+      .flat()
+      .sort((a, b) => {
+        const dateA = new Date(a.create_at || "").getTime();
+        const dateB = new Date(b.create_at || "").getTime();
+        return dateB - dateA;
+      });
+
+    const repositorys = gitData.sort((a, b) => {
+      const dateA = new Date(a.updated_at || "").getTime();
+      const dateB = new Date(b.updated_at || "").getTime();
+      return dateB - dateA;
+    });
+
+    setCommits(commits);
+    setRepos(repositorys);
+  }, [gitData]);
+
+  function handleClick(newSelect: Select) {
+    setSelect(newSelect);
+    setPage(1);
+  }
+
+  const displayedData =
+    select === "Commits"
+      ? commits.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+      : repos.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+
+  // https://mui.com/material-ui/react-pagination/
+  const pageHandleChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+
+  return (
+    <div className="mt-24 flex flex-col">
+      <Breadcrumbs aria-label="breadcrumb">
+        <Button
+          onClick={() => handleClick("Commits")}
+          color={select === "Commits" ? "primary" : "inherit"}
+          className="text-sm font-semibold"
+        >
+          Commits
+        </Button>
+        <Button
+          onClick={() => handleClick("Repository")}
+          color={select === "Repository" ? "primary" : "inherit"}
+          className="text-sm font-semibold"
+        >
+          Repository
+        </Button>
+      </Breadcrumbs>
+      <div className="w-max grid grid-cols-1 grid-flow-row-6 gap-x-4 gap-y-4 mt-2 lg:grid-cols-2 lg:grid-flow-row-3">
+        {displayedData.map((data, idx) => {
+          if (select === "Commits") {
+            const typeData = data as Commit;
+            return (
+              <GitHubLabel
+                key={idx}
+                title={typeData.message.split('\n')[0]}
+                createAt={typeData.create_at ? typeData.create_at.replace("T", " ").replace("Z", "") : ""}
+                url={typeData.html_url}
+              />
+            );
+          } else {
+            const typeData = data as Repository;
+            return (
+              <GitHubLabel
+                key={idx}
+                title={typeData.name}
+                createAt={typeData.updated_at ? typeData.updated_at.replace("T", " ").replace("Z", "") : ""}
+                url={typeData.html_url}
+              />
+            );
+          }
+        })}
+      </div>
+      <Pagination
+        count={Math.ceil(
+          (select === "Commits" ? commits.length : repos.length) / itemsPerPage
+        )}
+        page={page}
+        onChange={pageHandleChange}
+        className="mt-4 text-2xl"
+      />
+    </div>
+  );
+}
