60e3f8b2519d73910b86056e4a65b3da064328e4
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-01 22:51:11 +0900

    Feat | GitStateProvicder를 Body위에 감싸여, 모든 Component에서 context를 활용할 수 있게함

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { GitStateProvider } from "../../context/git_state.context";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -25,11 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <GitStateProvider>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {children}
+          </body>
+        </GitStateProvider>
     </html>
   );
 }
