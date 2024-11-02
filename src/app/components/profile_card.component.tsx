57463aae297a05a34ab73cc3ea3cdc1db993530a
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-02 15:00:46 +0900

    New | 프로필 카드 컴포넌트, 스켈레톤 작업 완료

---
+++ src/app/components/profile_card.component.tsx
@@ -0,0 +1,46 @@
+import {
+  Avatar,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+
+export function ProfileCard() {
+  return (
+    <Card
+      className="max-w-80 bg-[linear-gradient(0deg,rgba(235,217,217,0)_0%,rgba(27,205,166,0.2)_100%)] 
+            backdrop-blur-lg rounded-sm mt-52"
+    >
+      <CardHeader
+        avatar={<Avatar alt="avartar" src="/avartar.jpeg" />}
+        title="Hwang Jae Min"
+        subheader="ghkdwoals1005@gmail.com"
+      />
+      <CardContent>
+        <Typography variant="subtitle2">Readme</Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton>
+            <HomeOutlinedIcon/>
+        </IconButton>
+        <IconButton>
+            <MailOutlineIcon/>
+        </IconButton>
+        <IconButton>
+            <GitHubIcon/>
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
