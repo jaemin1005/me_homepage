d30606c1071a1434dd01d94857c0ede376b24280
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-02 13:20:40 +0900

    Feat | 레포 정보를 서로 다른 컴포넌트들끼리 전달하기 위해, Provider 설정

--- src/app/components/github.component.tsx
+++ src/app/components/github.component.tsx
@@ -1,11 +1,14 @@
+import { SelectRepoProvider } from "../../../context/select_repo.context";
 import { Chart } from "./chart.component";
 import { GitHubInfo } from "./github_info.component";
 
-export function GitHub(){
-    return (
-        <div className="w-full h-screen px-20 pt-52">
-            <Chart/>
-            <GitHubInfo/>
-        </div>
-    )    
+export function GitHub() {
+  return (
+    <div className="w-full h-screen px-20 pt-52 relative">
+      <SelectRepoProvider>
+        <Chart />
+        <GitHubInfo />
+      </SelectRepoProvider>
+    </div>
+  );
 }
