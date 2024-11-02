8a7fee068ef504dac1f17ffca08059f1a011c514
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-02 13:15:46 +0900

    New | 선택한 레포지토리를 다른 컴포넌트에 전달하기 위한 context
    선택한 레포를 그래프에 반영할 수 있게 전달

---
+++ context/select_repo.context.tsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState } from "react";
+
+interface SelectRepoContextProps {
+  name: string | undefined;
+  setName: (name: string | undefined) => void;
+}
+
+const selectRepoContext = createContext<SelectRepoContextProps>({
+    name: undefined,
+    setName: () => {}
+  });
+  
+  export const SelectRepoProvider = ({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) => {
+    const [name, setName] = useState<string>();
+  
+    return (
+      <selectRepoContext.Provider value={{ name, setName }}>
+        {children}
+      </selectRepoContext.Provider>
+    );
+  };
+  
+  export const useRepoState = () => useContext(selectRepoContext);
