d9d720f3b17f3e997e09a9648010123d96334dfd
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-04 19:29:49 +0900

    Style | rounded 통일

--- src/app/components/profile_card.component.tsx
+++ src/app/components/profile_card.component.tsx
@@ -56,7 +56,7 @@ export function ProfileCard({ children }: { children: React.ReactNode }) {
     <>
       <Card
         className="max-w-80 bg-[linear-gradient(0deg,rgba(235,217,217,0)_0%,rgba(41,141,146,0.2)_100%)] 
-            backdrop-blur-lg rounded-md mt-36 sticky top-36"
+            backdrop-blur-lg rounded-[20px] mt-36 sticky top-36"
       >
         <CardHeader
           avatar={<Avatar alt="avartar" src="/avartar.jpeg" />}
