19e0eec25437b4ab91699896b6f090f0dde7bceb
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-04 19:31:50 +0900

    Style | 가로축 패딩 삭제

--- src/app/components/github.component.tsx
+++ src/app/components/github.component.tsx
@@ -4,7 +4,7 @@ import { GitHubInfo } from "./github_info.component";
 
 export function GitHub() {
   return (
-    <div className="w-full h-screen px-20 pt-52 relative">
+    <div className="w-full h-screen pt-52 relative">
       <SelectRepoProvider>
         <Chart />
         <GitHubInfo />
