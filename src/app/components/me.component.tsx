2276954fb81b4b7a9db06481cba37d71f973c46d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-04 19:45:29 +0900

    Fix | 기술 스택을 분리하고 나에 대한 정보를 담긴 Component로 변경

--- src/app/components/me.component.tsx
+++ src/app/components/me.component.tsx
@@ -1,36 +1,41 @@
+import {
+  SelectMeProvider,
+  useMeState,
+} from "../../../context/select_me.context";
+import { MeDetail } from "./me_components/me_detail.component";
+import {
+  COMMUNICATION_ME,
+  DRIVING_FORCE_ME,
+  PLANNING_SKILL,
+  PROBLEM_SOLVING_SKILL,
+} from "./me_components/me_info_objects";
+import { MePieChart } from "./me_components/pie_chart.component";
 import { MeContainer } from "./me_container.component";
 
 export function Me() {
   return (
-    <div className="w-screen min-h-screen px-20 py-36 flex flex-col justify-between">
-      <h1
-        className="font-inter font-bold text-8xl leading-[116px] tracking-[0.1em] 
+    <SelectMeProvider>
+      <div className="w-full h-screen pt-36 flex flex-col">
+        <h4
+          className="font-inter font-bold text-6xl tracking-[0.1em] 
+           text-[#8F9795] py-0 text-wrap"
+        >
+          IT's ME
+        </h4>
+        <h1
+          className="font-inter font-bold text-8xl tracking-[0.1em] 
            bg-gradient-to-r from-[#8F9795] to-[#5EA196] text-transparent 
-           bg-clip-text"
-      >
-        PORTFOLIO
-      </h1>
-      <div>
-        <MeContainer
-          title="LANGUAGE"
-          tags={["javascript", "typescript", "rust", "C#"]}
-        />
-        <MeContainer
-          title="FRAMEWORK"
-          tags={["Nextjs", "Nestjs", "Actix Web", "Tauri", "Leptos", "Express"]}
-        />
-        <MeContainer
-          title="TOOLS"
-          tags={[
-            "AWS RDS",
-            "AWS LAMBDA",
-            "AWS DYNAMODB",
-            "AWS EC2",
-            "AWS S3",
-          ]}
-        />
-        <MeContainer tags={["AWS Amplify","Web Assembly", "Docker", "GIT"]}/>
+           bg-clip-text overflow-hidden"
+        >
+          HOMEPAGE
+        </h1>
+        <div className="flex flex-col w-full h-full items-center gap-x-10 sm:flex-row">
+          <div className="flex-shrink-0">
+            <MePieChart />
+          </div>
+          <MeDetail />
+        </div>
       </div>
-    </div>
+    </SelectMeProvider>
   );
 }
