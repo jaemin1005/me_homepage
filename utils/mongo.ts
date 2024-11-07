641ad7533c3e1272f600ee66ff50c50d235c3a04
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-07 10:30:27 +0900

    Feat | MongoDBClientOptions 수정
    쓰기, 읽기 재시도 옵션 추가 및 연결 시간 설정

--- utils/mongo.ts
+++ utils/mongo.ts
@@ -1,6 +1,6 @@
 /**
  * 전역 변수에 mongodb client 선언
- * Aamplify Vercel 같은 환경에서는 백앤드 API들이 서버리스 환경에서 실행되게 됨
+ * Amplify, Vercel 같은 환경에서는 백엔드 API들이 서버리스 환경에서 실행되게 됨
  * 함수가 별도의 인스턴스로 실행될 수 있으며, 인스턴스 간에 상태나 연결이 자동으로 유지되지 않기 때문에
  * 전역으로 선언한다.
  */
@@ -8,25 +8,27 @@ import { MongoClient, MongoClientOptions } from "mongodb";
 
 const uri: string = process.env.MONGODB_URI!;
 const options: MongoClientOptions = {
-  maxPoolSize: 10,
+  maxPoolSize: 1, // 서버리스 환경에서는 작은 풀 사이즈가 적합
+  minPoolSize: 1, // 최소 풀 사이즈도 1로 설정
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  connectTimeoutMS: 10000,
+  retryWrites: true,
+  retryReads: true
 };
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-// 전역 선언
+// 전역 타입 선언
 declare global {
   // eslint-disable-next-line no-var
-  var _mongoClientPromise: Promise<MongoClient>;
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-// 전역에 해당 Client를 등록한다.
+// 개발 환경에서 핫 리로딩 시 연결이 재사용되도록 전역 캐싱
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
+  const client = new MongoClient(uri, options);
   global._mongoClientPromise = client.connect();
 }
 
-// eslint-disable-next-line prefer-const
-clientPromise = global._mongoClientPromise;
+const clientPromise = global._mongoClientPromise!;
 
 export default clientPromise;
