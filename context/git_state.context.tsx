8eea3b316d7bc8cf9a52a2a91739f02aa11af268
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-07 16:16:00 +0900

    Feat | MongoDB와의 연결이 실패할 떄 S3에 있는 파일을 불러온다

--- context/git_state.context.tsx
+++ context/git_state.context.tsx
@@ -5,6 +5,25 @@ import { Repository } from "../interfaces/git_state.interface";
 
 const gitStateContext = createContext<Repository[]>([]);
 
+const getS3Data = async () => {
+  try {
+    const res = await fetch("api/s3");
+
+    if (!res.ok) {
+      const errorResult = await res.json();
+      console.error("API S3 Error:", errorResult);
+      throw new Error(
+        `API responded with status: ${res.status} ${res.statusText}`
+      );
+    }
+
+    const result = await res.json();
+    return result.repositories as Repository[];
+  } catch {
+    throw new Error("API S3 Error");
+  }
+};
+
 export const GitStateProvider = ({
   children,
 }: {
@@ -21,10 +40,14 @@ export const GitStateProvider = ({
         if (res.ok) {
           setData(result.repositories);
         } else {
-          console.error("API Error", result);
+          console.error("API Repositories Error", result);
+          const s3Data = await getS3Data();
+          setData(s3Data);
         }
       } catch (error) {
         console.error("Network Error:", error);
+        const s3Data = await getS3Data();
+        setData(s3Data);
       }
     };
     fetchData();
