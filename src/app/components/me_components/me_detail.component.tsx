19a3ca83e3abdcf3b636949b9ea53a6583091a74
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-08 16:40:56 +0900

    Style | Font 사이즈 및 색깔을 수정하였다

--- src/app/components/me_components/me_detail.component.tsx
+++ src/app/components/me_components/me_detail.component.tsx
@@ -1,33 +1,25 @@
-import { useEffect, useMemo, useState } from "react";
-import { useMeState } from "../../../../context/select_me.context";
-import { DEFAULT_ME } from "./me_info_objects";
+import { Typography } from "@mui/material";
 
-export function MeDetail() {
-  const defaultData = useMemo(() => {
-    return DEFAULT_ME;
-  }, []);
-
-  const { meInfo } = useMeState();
-  const [curInfo, setCurInfo] = useState<MeInfo>(DEFAULT_ME);
-
-  useEffect(() => {
-    if (meInfo === undefined) {
-      setCurInfo(defaultData);
-    } else {
-      setCurInfo(meInfo);
-    }
-  }, [meInfo]);
-
+export function MeDetail({ meInfo }: { meInfo: MeInfo }) {
   return (
     <>
       <div className="w-full scrollbar-hidden overflow-y-scroll">
-        <h1 className="font-inter font-bold text-2xl text-[#298D92] leading-none my-0">
-          {curInfo.title}
-        </h1>
-        <p
+        {/* <h1 className="font-inter font-bold text-2xl text-[#298D92] leading-none my-0">
+          {meInfo.title}
+        </h1> */}
+        <Typography variant="subtitle1" className="font-bold">
+          {meInfo.title}
+        </Typography>
+        {/* <p
           className="font-inter font-bold text-lg text-[#B6AFAF] hover:text-[#e0dddd] max-w-[550px] text-justify"
-          dangerouslySetInnerHTML={{ __html: curInfo.body }}
-        ></p>
+          dangerouslySetInnerHTML={{ __html: meInfo.body }}
+        ></p> */}
+        <Typography
+          variant="body1"
+          className="max-w-[550px] text-justify  text-[#B6AFAF]"
+        >
+          {meInfo.body}
+        </Typography>
       </div>
     </>
   );
