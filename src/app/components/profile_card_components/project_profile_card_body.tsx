713b657dc2dc69417302fe46dd02d0fb46ee7268
jaemin1005 <ghkdwoals1005@gmail.com>
2024-11-09 18:41:54 +0900

    Style | 문자열로 작성된 문구, 오타 검사 및 맞춤법 검사 완료 내용을 다듬었다.

--- src/app/components/profile_card_components/project_profile_card_body.tsx
+++ src/app/components/profile_card_components/project_profile_card_body.tsx
@@ -5,10 +5,11 @@ export function ProjectProfileCardBody() {
     <>
       <Typography variant="subtitle2">Comment</Typography>
       <Typography variant="body2" sx={{ color: "text.secondary" }}>
-        개인 프로젝트는 모든 프론트엔드 및 백엔드가 AWS에 배포되어 있는
-        상태입니다. 팀 프로젝트의 경우, 배포가 완료되지 않은 부분들은 영상으로
-        대체하여 설명하였습니다. 이를 통해 프로젝트의 기능과 결과물을 효과적으로
-        시연할 수 있도록 하였습니다.
+        개인 프로젝트는 모든 프론트엔드와 백엔드가 AWS에 배포되어 있으며, 이를
+        통해 실제 운영 환경에서의 성능과 안정성을 검증할 수 있었습니다. 팀
+        프로젝트의 경우, 배포가 완료되지 않은 기능들은 영상으로 대체하여
+        설명하였습니다. 현재 진행하고 있는 작업은 THEME MAP 프로젝트의 리팩토링
+        작업과 기능 개선, 추가를 하고 있습니다.
       </Typography>
     </>
   );
--- src/app/components/profile_card_components/skill_profile_card_body.tsx
+++ src/app/components/profile_card_components/skill_profile_card_body.tsx
@@ -5,10 +5,12 @@ export function SkillProfileCardBody() {
     <>
       <Typography variant="subtitle2">Comment</Typography>
       <Typography variant="body2" sx={{ color: "text.secondary" }}>
-        제가 지금까지 사용하면서 프로젝트에 적용해온 
-        언어 및 프레임워크를 평가해본 지표들입니다. 각 1, 2, 3등 순위를 매겨
-        나눠놓았습니다. 지금 현재 학습하고 있는 것은 쿠버네티스와 AWS ECS를 학습하고 있습니다.
-        또한 DENO 2.0을 기존의 NodeJs의 차이를 비교하면서 테스트하고 있습니다. 
+        제가 프로젝트에 적용해 온 언어 및 프레임워크를 평가한 지표입니다. 각
+        항목에 대해 1위, 2위, 3위로 순위를 매겼으며, 이를 통해 각 기술의 강점과
+        약점을 파악하고 향후 프로젝트에 어떻게 활용할지를 고민하고 있습니다.
+        현재는 쿠버네티스와 AWS ECS를 학습하여 클라우드 기반의 컨테이너 관리에
+        대한 이해를 높이고 있으며, Deno 2.0을 기존의 Node.js와 비교하여
+        테스트하고 있습니다.
       </Typography>
     </>
   );
